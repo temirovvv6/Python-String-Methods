7a9ca7b780a748f005497d7c4d01600bfac65c99
temirovvv6 <temirovvv38@gmail.com>
2025-12-11 17:54:23 +0500

    task28 bajarildi

---
+++ task28.py
@@ -0,0 +1,8 @@
+izoh1 = input("Izoh 1 kiriting: ")
+izoh2 = input("Izoh 2 kiriting: ")
+
+izoh1_tozalangan = izoh1.replace("yomon", "").strip()
+izoh2_tozalangan = izoh2.replace("yomon", "").strip()
+
+print(izoh1_tozalangan)
+print(izoh2_tozalangan)
